7a104eec6a0f0d10a3f4df24aaf648f3b5ac8bce
sunil poojari <sunil.poojari217@siesgst.ac.in>
2020-02-02 11:11:40 +0530

    Changes

--- public/js/demo.js
+++ public/js/demo.js
@@ -1,15 +1,15 @@
 function initMap() {
   var center = {lat: 19.06594216466549, lng: 73.00267323751609};
   var locations = [
-    ['Slum 1<br>\
+    ['Slum 1 <br>Donated Food:1 day Ago.<br>\
     <input style="width:100%" class="form-control btn btn-primary" id="submit" name="submit" type="submit" value="Donate Food Here" data-toggle="modal" data-target="#nearbyvolunteers">', 19.2183072435819, 72.83263216143747],
-    ['Slum 2<br>\
+    ['Slum 2<br> Donated Food:2 day Ago.<br>\
     <input style="width:100%" class="form-control btn btn-primary" id="submit" name="submit" type="submit" value="Donate Food Here" data-toggle="modal" data-target="#nearbyvolunteers">', 19.066302172508813, 73.00299433382882],
-    ['Slum 3<br>\
+    ['Slum 3<br> Donated Food:3 day Ago.<br>\
     <input style="width:100%" class="form-control btn btn-primary" id="submit" name="submit" type="submit" value="Donate Food Here" data-toggle="modal" data-target="#nearbyvolunteers">', 19.06594216466549, 73.00267323751609],
-    ['Slum 4<br>\
+    ['Slum 4<br>Donated Food:5 day Ago.<br>\
     <input style="width:100%" class="form-control btn btn-primary" id="submit" name="submit" type="submit" value="Donate Food Here" data-toggle="modal" data-target="#nearbyvolunteers">', 19.066626632624107, 73.00187930364768],
-    ['Slum 5<br>\
+    ['Slum 5<br> Donated Food:12 Hrs Ago.<br>\
     <input style="width:100%" class="form-control btn btn-primary" id="submit" name="submit" type="submit" value="Donate Food Here" data-toggle="modal" data-target="#nearbyvolunteers">', 19.066829437402454, 73.00272688169639]
   ];
 var map = new google.maps.Map(document.getElementById('map'), {
